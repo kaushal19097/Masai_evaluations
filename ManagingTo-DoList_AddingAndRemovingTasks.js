ca8ca1d33579450a251b0eb2d4d67e33639426c1
Kaushal Chaudhary <71766570+kaushal19097@users.noreply.github.com>
2024-10-17 18:22:08 +0530

    Create ManagingTo-DoList_AddingAndRemovingTasks.js

---
+++ ManagingTo-DoList_AddingAndRemovingTasks.js
@@ -0,0 +1,9 @@
+// Managing a To-Do List: Adding and Removing Tasks
+let toDoList = ['Complete assignment', 'Go for a walk', 'Practise Coding', 'Learn Bash Scripting', 'Read a book'];
+
+toDoList.unshift('Attend meeting');
+toDoList.pop();
+for(let i in toDoList){
+console.log(toDoList[i]);
+  
+}

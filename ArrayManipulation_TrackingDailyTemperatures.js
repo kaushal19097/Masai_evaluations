1b82ee606ce94bf31d1e1cef9b1d5be238a6a04d
Kaushal Chaudhary <71766570+kaushal19097@users.noreply.github.com>
2024-11-11 14:11:26 +0530

    Delete ArrayManipulation_TrackingDailyTemperatures.js

--- ArrayManipulation_TrackingDailyTemperatures.js
+++
@@ -1,8 +0,0 @@
-// Array Manipulation: Tracking Daily Temperatures
-
-let temperatures = [30, 32, 31, 29, 33, 34, 30];
-temperatures[3] = temperatures[3] + 2;
-for(let i in temperatures)
-{
-console.log(temperatures[i]);  
-}

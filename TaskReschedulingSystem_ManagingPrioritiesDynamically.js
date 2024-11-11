741733b425ca06cb8869b937b5a0ca4310136685
Kaushal Chaudhary <71766570+kaushal19097@users.noreply.github.com>
2024-11-11 14:11:42 +0530

    Delete TaskReschedulingSystem_ManagingPrioritiesDynamically.js

--- TaskReschedulingSystem_ManagingPrioritiesDynamically.js
+++
@@ -1,8 +0,0 @@
-// L1 - Task Rescheduling System: Managing Priorities Dynamically
-let tasks = ['Complete assignment', 'Go for a walk', 'Practise Coding', 'Learn Bash Scripting', 'Read a book'];
-tasks.shift();
-tasks.unshift('Fix critical bug', 'Prepare presentation');
-tasks[tasks.length - 1] = 'Update project documentation';
-for (let i in tasks){
-console.log(tasks[i]);
-}
